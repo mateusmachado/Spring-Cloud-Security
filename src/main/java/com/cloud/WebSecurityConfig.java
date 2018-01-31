2b168725e5e3a2416df6923d4fed9b3bc0b8e547
Mateus <mmachado.csp@gmail.com>
2018-01-31 08:28:36 -0200

    initial commit

---
+++ src/main/java/com/cloud/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.cloud;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user1").password("password1").roles("USER");
+	}
+
+}
