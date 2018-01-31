15c30d636132ca6e211617075fa19029b65016d3
Mateus <mmachado.csp@gmail.com>
2018-01-31 09:47:26 -0200

    OAuth Resource Server Setup

--- src/main/java/com/cloud/SpringMicroservicesOauthServerApplication.java
+++ src/main/java/com/cloud/SpringMicroservicesOauthServerApplication.java
@@ -2,13 +2,28 @@ package com.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableAuthorizationServer
+@EnableResourceServer
+@RestController
+@EnableGlobalMethodSecurity(prePostEnabled=true)
 public class SpringMicroservicesOauthServerApplication {
+
+	@RequestMapping("/resource/endpoint")
+	@PreAuthorize("hasRole('ADMIN')")
+	public String endpoint() {
+		return "Message, resource server!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMicroservicesOauthServerApplication.class, args);
 	}
+
 }
